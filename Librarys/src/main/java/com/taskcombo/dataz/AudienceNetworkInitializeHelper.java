09ddf94a0d6777c3a1803f2674a572ed9fc9ab2e
Appcompato <sonalranaags@gmail.com>
2023-04-03 23:32:32 +0530

    2.0.0

---
+++ Librarys/src/main/java/com/taskcombo/dataz/AudienceNetworkInitializeHelper.java
@@ -0,0 +1,33 @@
+package com.taskcombo.dataz;
+
+import static com.facebook.ads.BuildConfig.DEBUG;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.facebook.ads.AdSettings;
+import com.facebook.ads.AudienceNetworkAds;
+
+public class AudienceNetworkInitializeHelper
+        implements AudienceNetworkAds.InitListener {
+
+    static void initialize(Context context) {
+        if (!AudienceNetworkAds.isInitialized(context)) {
+            if (DEBUG) {
+                AdSettings.turnOnSDKDebugger(context);
+            }
+
+            AudienceNetworkAds
+                    .buildInitSettings(context)
+                    .withInitListener(new AudienceNetworkInitializeHelper())
+                    .initialize();
+        }
+    }
+
+    @Override
+    public void onInitialized(AudienceNetworkAds.InitResult result) {
+        Log.d(AudienceNetworkAds.TAG, "thyu ke nai"+result.getMessage());
+    }
+
+
+}
---
+++ app/src/main/java/com/taskcombo/unirty/MActivity.java
@@ -0,0 +1,18 @@
+package com.taskcombo.unirty;
+
+import static com.taskcombo.dataz.Master_Actuators.ConstraintLayout_Prams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m);
+
+    }
+}
